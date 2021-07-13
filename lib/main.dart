0192be1b868582de5d51e54777c362601528a220
Vasiliy <49184825+w0u0w@users.noreply.github.com>
2021-07-13 16:57:48 +0300

    working audio player v0.03 new design and time slider

--- lib/main.dart
+++ lib/main.dart
@@ -31,25 +31,53 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   AudioPlayer _audioPlayer = AudioPlayer();
+  late AudioCache cache;
+
   bool isPlaying = false;
   bool filePathExists = false;
 
   String crtTime = "0:00:00";
   String cptTime = "0:00:00";
 
+  String songTitle = "...";
+
+  Duration position = new Duration();
+  Duration musicLength = new Duration();
+
+  Widget slider() {
+    return Slider.adaptive(
+        activeColor: Colors.lightBlue,
+        inactiveColor: Colors.grey,
+        value: position.inSeconds.toDouble(),
+        max: musicLength.inSeconds.toDouble(),
+        onChanged: (value) {
+          seekToSec(value.toInt());
+        });
+  }
+
+  void seekToSec(int sec) {
+    Duration newPos = Duration(seconds: sec);
+    _audioPlayer.seek(newPos);
+  }
+
   @override
   void initState() {
     super.initState();
-    
+
+    cache = AudioCache(fixedPlayer: _audioPlayer);
+
     _audioPlayer.onAudioPositionChanged.listen((Duration duration) {
       setState(() {
         crtTime = duration.toString().split(".")[0];
+        position = duration;
+        print(position);
       });
     });
 
     _audioPlayer.onDurationChanged.listen((Duration duration) {
       setState(() {
-        cptTime  = duration.toString().split(".")[0];
+        cptTime = duration.toString().split(".")[0];
+        musicLength = duration;
       });
     });
   }
@@ -57,78 +85,164 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo,
-      body: Stack(
-        children: [
-          Image.asset(
-            "assets/bg1.gif",
-            fit: BoxFit.contain,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: 80,
-            margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.7,
-                left: MediaQuery.of(context).size.width * 0.1),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(30)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.blue.shade800,
+                Colors.blue.shade200,
+              ]),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 48.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(
-                  onPressed: () {
-
-                    if (isPlaying) {
-                      _audioPlayer.pause();
-
-                      setState(() {
-                        isPlaying = false;
-                      });
-                    } else if(filePathExists && !isPlaying){
-                      _audioPlayer.resume();
-
-                      setState(() {
-                        isPlaying = true;
-                      });
-                    } else if(!filePathExists && !isPlaying){
-                      Toast.show(
-                        "Choose audio file",
-                        context,
-                        duration: Toast.LENGTH_LONG,
-                          gravity:  Toast.BOTTOM
-                      );
-                    }
-                  },
-                  icon: Icon(isPlaying
-                      ? Icons.pause_rounded
-                      : Icons.play_arrow_rounded),
-                  iconSize: 32.0,
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    "Now playing",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                SizedBox(width: 25),
-                IconButton(
-                  onPressed: () {
-                    _audioPlayer.stop();
-                    crtTime = "0:00:00";
-                    setState(() {
-                      isPlaying = false;
-                    });
-                  },
-                  icon: Icon(Icons.stop_rounded),
-                  iconSize: 32.0,
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: Text(
+                    songTitle,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w400),
+                  ),
                 ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                Center(
+                  child: Container(
+                    width: 280.0,
+                    height: 280.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage("assets/bg1.gif"),
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25.0),
+                        topRight: Radius.circular(25.0),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: Text(
+                                crtTime,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 12.0),
+                              child: Text(
+                                cptTime,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        slider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.skip_previous_rounded),
+                              iconSize: 40.0,
+                              color: Colors.lightBlue,
+                            ),
+                            SizedBox(width: 20),
+                            IconButton(
+                              onPressed: () {
+                                if (isPlaying) {
+                                  _audioPlayer.pause();
 
-                SizedBox(width: 20),
+                                  setState(() {
+                                    isPlaying = false;
+                                  });
+                                } else if (filePathExists && !isPlaying) {
+                                  _audioPlayer.resume();
 
-                Text(crtTime, style: TextStyle(fontWeight: FontWeight.w700),),
-
-                Text("  |  "),
-
-                Text(cptTime, style: TextStyle(fontWeight: FontWeight.w300),),
+                                  setState(() {
+                                    isPlaying = true;
+                                  });
+                                } else if (!filePathExists && !isPlaying) {
+                                  Toast.show("Choose audio file", context,
+                                      duration: Toast.LENGTH_LONG,
+                                      gravity: Toast.BOTTOM);
+                                }
+                              },
+                              icon: Icon(isPlaying
+                                  ? Icons.pause_rounded
+                                  : Icons.play_arrow_rounded),
+                              iconSize: 40.0,
+                              color: Colors.blueAccent,
+                            ),
+                            SizedBox(width: 20),
+                            IconButton(
+                              onPressed: () {
+                                _audioPlayer.stop();
+                                crtTime = "0:00:00";
+                                setState(() {
+                                  isPlaying = false;
+                                });
+                              },
+                              icon: Icon(Icons.stop_rounded),
+                              iconSize: 40.0,
+                              color: Colors.blueAccent,
+                            ),
+                            SizedBox(width: 20),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.skip_next_rounded),
+                              color: Colors.lightBlue,
+                              iconSize: 40.0,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
-        ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.audiotrack_rounded),
@@ -136,7 +250,8 @@ class _HomePageState extends State<HomePage> {
           FilePickerResult? result = await FilePicker.platform.pickFiles();
           if (result != null) {
             PlatformFile file = result.files.first;
-            print(file.path);
+            songTitle = file.name.toString();
+
             int status =
                 await _audioPlayer.play(file.path.toString(), isLocal: true);
             if (status == 1) {
@@ -144,8 +259,7 @@ class _HomePageState extends State<HomePage> {
                 isPlaying = true;
                 filePathExists = true;
               });
-            }
-            else {
+            } else {
               setState(() {
                 isPlaying = false;
                 filePathExists = false;
@@ -154,6 +268,7 @@ class _HomePageState extends State<HomePage> {
           }
         },
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
     );
   }
 }
